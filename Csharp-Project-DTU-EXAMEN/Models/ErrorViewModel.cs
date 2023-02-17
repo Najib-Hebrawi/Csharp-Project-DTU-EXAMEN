727a1f5e69270bbe995d0ee1a68650a3c25f10f4
NajibHebrawi <NajibHebrawi@hotmail.com>
2023-02-17 16:31:50 +0100

    myFIrstCommit

---
+++ Csharp-Project-DTU-EXAMEN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Csharp_Project_DTU_EXAMEN.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
